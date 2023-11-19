3abcae2129ece602929cc2c30e3b1450b349dc97
JarPr <131611487+JarPr@users.noreply.github.com>
2023-11-19 15:27:13 +0100

    react-hw-03-phonebook

---
+++ src/components/FormContacts/FormContacts.styled.js
@@ -0,0 +1,76 @@
+
+import styled from 'styled-components'
+
+ 
+export const ContainerDivPhonebook = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+`
+export const TitlePhonebook = styled.h2`
+font-family: 'roboto';
+font-size: 24px;
+`
+export const TitleContacts = styled.h2`
+font-family: 'roboto';
+font-size: 24px;
+`
+export const FormPhonebook = styled.form`
+width: 300px;
+height: 250px;
+border: 2px solid #558B2F;
+padding: 20px;
+margin-bottom: 30px;
+`
+   export const LabelPhonebook = styled.label`
+font-family: 'roboto';
+
+   `
+export const TitleInputSearch = styled.h3`
+font-family: 'roboto';
+font-size: 20px;
+`
+export const InputPhonebookName = styled.input`
+   display: block;
+   margin-top: 10px;
+   margin-bottom: 50px;
+   border: 2px solid #558B2F;
+      height: 20px;
+   width: 300px;
+   `
+export const InputPhonebookNumber = styled.input`
+   display: block;
+   margin-top: 10px;
+   margin-bottom: 50px;
+   border: 2px solid #558B2F;
+     height: 20px;
+   width: 300px;
+   `
+
+
+export const ButtonPhonebook = styled.button`
+color: #33691E;
+font-family: 'roboto';
+border: 2px solid #558B2F;
+font-size: 16px;
+background-color: #CCFF90;
+width: 300px;
+`
+
+export const InputSearch = styled.input`
+width: 185px;
+border: 2px solid #000;
+margin-bottom: 15px;
+`
+export const ButtonDelete = styled.button`
+border: 1px solid #000;
+background-color: #BDBDBD;
+margin-top: 10px;
+`
+export const  ItemContact = styled.li`
+font-family: 'roboto';
+margin-bottom: 15px;
+display: flex;
+flex-direction: column;
+`
